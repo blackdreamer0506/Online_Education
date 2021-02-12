67d2ca6dcbb0cfacd882bcbfcb2cb4f9b4d3b12e
Xiao Ye <yexiao@Xiaos-MacBook-Pro.local>
2021-02-11 21:03:02 -0500

    add delete by id

--- online-education/Service/eduservice/src/main/java/com/xiao/edu/configs/EduConfig.java
+++ online-education/Service/eduservice/src/main/java/com/xiao/edu/configs/EduConfig.java
@@ -1,9 +1,16 @@
 package com.xiao.edu.configs;
 
+import com.baomidou.mybatisplus.core.injector.ISqlInjector;
+import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @MapperScan("com.xiao.edu.mappers")
 public class EduConfig {
+    @Bean
+    public ISqlInjector sqlInjector() {
+        return new LogicSqlInjector();
+    }
 }
